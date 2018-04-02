1dab59f289a71b12495d165bf3556581097fdef9
F0lha <pedrovieiradcastro@gmail.com>
2018-04-02 20:26:01 +0100

    fix classification

--- DeepLearningModel.cpp
+++ DeepLearningModel.cpp
@@ -10,7 +10,7 @@ DeepLearningModel::DeepLearningModel() :
 DeepLearningModel::DeepLearningModel(int features, int nclasses) :
     nclasses{ nclasses }, 
     params{ new CvANN_MLP_TrainParams(
-        cvTermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 2000, 0.000001),
+        cvTermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 1000, 0.000001),
         // use backpropogation for training
         CvANN_MLP_TrainParams::BACKPROP,
         // co-efficents for backpropogation training
@@ -21,17 +21,18 @@ DeepLearningModel::DeepLearningModel(int features, int nclasses) :
     model.reset(new CvANN_MLP());
     Mat layers = Mat(5,1, CV_32SC1);
 	layers.at<int>(0, 0) = features;     // input
-	layers.at<int>(1, 0) = 64;  // hidden
-	layers.at<int>(2, 0) = 32;  // hidden
-	layers.at<int>(3, 0) = 32;  // hidden
-	layers.at<int>(4, 0) = nclasses;      // output, 1 pin per class.
-	static_cast<CvANN_MLP*>(model.get())->create(layers);
+	layers.at<int>(1, 0) = 128;  // hidden
+	layers.at<int>(2, 0) = 64;  // hidden
+	layers.at<int>(3, 0) = 64;  // hidden
+	layers.at<int>(4, 0) = this->nclasses;      // output, 1 pin per class.
+	static_cast<CvANN_MLP*>(model.get())->create(layers, CvANN_MLP::SIGMOID_SYM, 1, 1);
 }
 
 void DeepLearningModel::train(Mat training_data, vector<int> label) {
-	Mat train_classes = Mat::zeros(training_data.rows, nclasses, CV_32FC1);
-	for (int i = 0; i<train_classes.rows; i++) {
-		train_classes.at<float>(i, label.at(i)) = 1.f;
+	CvMat* train_classes = cvCreateMat(training_data.rows, this->nclasses, CV_32FC1);
+	cvZero(train_classes);
+	for (int i = 0; i < label.size(); i++) {
+		CV_MAT_ELEM(*train_classes, float, i, label[i]) = 1.0;
 	}
     static_cast<CvANN_MLP*>(model.get())->train(training_data, train_classes, Mat(), Mat(), (*params), 0);
 }
